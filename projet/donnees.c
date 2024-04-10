c1a427eed8137fa4b71b178a8a7bb60a2c5c025b
Mathias Burnel <burnel9u@aig-ats-315-l07.fst.site.univ-lorraine.fr>
2024-04-10 08:31:00 +0200

    Niveau2 ajout colisions + bord ecran

---
+++ projet/donnees.c
@@ -0,0 +1,242 @@
+
+
+#include"donnees.h"
+#include "constantes.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void init_sprite (sprite_t * sprite, int x, int y, int w, int h, int v, int alv) {
+    sprite->x = x;
+    sprite->y = y;
+    sprite->w = w; //largeur sprite
+    sprite->h = h; //hauteur sprite
+    sprite->v = v; //vitesse sprite
+    sprite->is_visible = 1;
+    sprite->is_alive=alv;
+}
+
+
+/**
+* \brief Procédure qui rend un sprite visible
+*/
+
+void set_visible (sprite_t * sprite) {
+    sprite->is_visible = 1;
+}
+
+
+/**
+* \brief Procédure qui rend un sprite invisible
+*/
+
+void set_invisible (sprite_t * sprite) {
+    sprite->is_visible = 0;
+}
+
+
+/**
+* \brief 
+*/
+void print_sprite (sprite_t * sprite) {
+    printf("Coordonnée x : %d\n", sprite->x);
+    printf("Coordonnée y : %d\n", sprite->y);
+    printf("Hauteur : %d\n", sprite->h);
+    printf("Largeur : %d\n", sprite->w);
+    printf("Vitesse : %d\n", sprite->v);
+    printf("en vie :%d\n ", sprite->is_alive);
+}
+
+/**
+ * \brief La fonction initialise les données du monde du jeu
+ * \param world les données du monde
+ */
+
+
+void init_data(world_t * world){
+    world->vaisseau = malloc(sizeof(sprite_t));
+    world->v_ennemi = malloc(sizeof(sprite_t));
+    world->missile = malloc(sizeof(sprite_t));
+
+    init_sprite (world->vaisseau, (SCREEN_WIDTH/2) - (SHIP_SIZE/2), SCREEN_HEIGHT- (SHIP_SIZE*3/2), SHIP_SIZE, SHIP_SIZE, world->vaisseau->v = 5,1);
+    print_sprite(world->vaisseau);
+    
+    init_sprite (world->v_ennemi, (SCREEN_WIDTH/2) - (SHIP_SIZE/2), (SHIP_SIZE/2), SHIP_SIZE, SHIP_SIZE, ENEMY_SPEED,1);
+
+    init_sprite (world->missile, world->vaisseau->x + (SHIP_SIZE/2) - (MISSILE_SIZE/2), world->vaisseau->y - (MISSILE_SIZE), SHIP_SIZE, SHIP_SIZE, MISSILE_SPEED,1);
+
+    set_invisible(world->missile);
+
+    //on n'est pas à la fin du jeu
+    world->gameover = 0;
+    
+}
+
+/**
+ * \brief La fonction nettoie les données du monde
+ * \param world les données du monde
+ */
+
+
+void clean_data(world_t *world){
+    free(world->vaisseau);
+    free(world->v_ennemi);
+    free(world->missile);
+    free(world);
+}
+
+
+/**
+ * \brief La fonction indique si le jeu est fini en fonction des données du monde
+ * \param world les données du monde
+ * \return 1 si le jeu est fini, 0 sinon
+ */
+
+int is_game_over(world_t *world){
+    return world->gameover;
+}
+
+void limite_ecran_joueur(world_t *world){
+
+    // Limite gauche
+    if (world->vaisseau->x < 0) {
+        world->vaisseau->x = 0;
+    }
+    
+    // Limite droite
+    if (world->vaisseau->x + world->vaisseau->w > SCREEN_WIDTH) {
+        world->vaisseau->x = SCREEN_WIDTH - world->vaisseau->w;
+    }
+}
+
+void limite_ecran_ennemi(world_t *world){
+
+    // Limite basse
+    if (world->v_ennemi->y > SCREEN_HEIGHT) {
+        world->v_ennemi->y = SHIP_SIZE/2;
+    }
+
+}
+
+int sprites_collide_rectangle(sprite_t *sp1, sprite_t *sp2) {
+    
+    float sp1_gauche = sp1->x;
+    float sp1_droite = sp1->x + sp1->w;
+    float sp1_haut = sp1->y;
+    float sp1_bas = sp1->y + sp1->h;
+
+    float sp2_gauche = sp2->x;
+    float sp2_droite = sp2->x + sp2->w;
+    float sp2_haut = sp2->y;
+    float sp2_bas = sp2->y + sp2->h;
+
+    // chevauchement
+    if (sp1_droite >= sp2_gauche && sp1_gauche <= sp2_droite && sp1_bas >= sp2_haut && sp1_haut <= sp2_bas) {
+        
+        return 1;
+    } 
+    return 0;
+    
+}
+
+int sprites_collide_cercle(sprite_t *sp1, sprite_t *sp2) {
+    
+    if (sp1->is_alive && sp2->is_alive){
+        // Calcul des coordonnées des centres des sprites
+        float sp1_centre_x = sp1->x + sp1->w / 2.0;
+        float sp1_centre_y = sp1->y + sp1->h / 2.0;
+        float sp2_centre_x = sp2->x + sp2->w / 2.0;
+        float sp2_centre_y = sp2->y + sp2->h / 2.0;
+
+        // Calcul de la distance entre les centres des sprites
+        float distance_centres = sqrt(pow(sp2_centre_x - sp1_centre_x, 2) + pow(sp2_centre_y - sp1_centre_y, 2));
+        
+        // Si distance entre les centres est <= somme des rayons alors chevauchement
+        float rayon_sp1 = sp1->w / 2.0;
+        float rayon_sp2 = sp2->w / 2.0;
+        if (distance_centres <= rayon_sp1 + rayon_sp2) {
+            printf("collision\n");
+            return 1; 
+        } 
+    }
+    
+    return 0;
+}
+ 
+
+void handle_sprites_collision(sprite_t *sp1, sprite_t *sp2) {
+    // Vérifier si les deux sprites sont encore visibles, en vie et en collision
+    if (sp1->is_visible && sp1->is_alive && sp2->is_visible && sp2->is_alive && sprites_collide_cercle(sp1, sp2)) {
+        sp1->is_alive = 0;
+        sp2->is_alive = 0;
+
+    }
+}
+
+/**
+ * \brief La fonction met à jour les données en tenant compte de la physique du monde
+ * \param les données du monde
+ */
+
+void update_data(world_t *world){
+    world->v_ennemi->y += world->v_ennemi->v;
+    if(world->missile->is_visible == 1) {
+        world->missile->y -= world->missile->v;
+    }
+
+    limite_ecran_joueur(world);
+    limite_ecran_ennemi(world);
+    
+    handle_sprites_collision(world->vaisseau,world->v_ennemi);
+    handle_sprites_collision(world->missile,world->v_ennemi);
+}
+
+
+
+
+
+/**
+ * \brief La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
+ * \param event paramètre qui contient les événements
+ * \param world les données du monde
+ */
+
+void handle_events(SDL_Event *event,world_t *world){
+    Uint8 *keystates;
+    while( SDL_PollEvent( event ) ) {
+        
+        //Si l'utilisateur a cliqué sur le X de la fenêtre
+        if( event->type == SDL_QUIT ) {
+            //On indique la fin du jeu
+            world->gameover = 1;
+        }
+
+        //Si l'utilisateur a cliqué sur le X de la fenêtre
+        if( event->type == SDL_KEYDOWN ) {
+            if(event->key.keysym.sym == SDLK_ESCAPE){
+                //si la touche appuyée est 'ESCAPE'
+                world->gameover = 1;
+            }
+        }
+
+        if(event->type == SDL_KEYDOWN){
+            //si la touche appuyée est 'D'
+            if(event->key.keysym.sym == SDLK_d){
+                world->vaisseau->x += world->vaisseau->v;
+            }
+        }
+
+        if(event->type == SDL_KEYDOWN){
+            //si la touche appuyée est 'G'
+            if(event->key.keysym.sym == SDLK_q){
+                world->vaisseau->x -= world->vaisseau->v;
+            }
+        }
+
+         // Si la touche appuyée est 'ESPACE' et que le vaisseau est visible
+        if (event->key.keysym.sym == SDLK_SPACE && world->vaisseau->is_visible) {
+            world->missile->x = world->vaisseau->x + SHIP_SIZE/2 - MISSILE_SIZE/2;
+            world->missile->is_visible = 1;
+        }
+    }
+}
