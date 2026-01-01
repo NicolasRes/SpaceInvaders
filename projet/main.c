94481fda633f1fc702aa2cac644485c7303a592b
resende3u <nicolas.resende5@etu.univ-lorraine.fr>
2026-01-01 18:18:25 +0100

    Game loop changed to handle different states of the game

--- projet/main.c
+++ projet/main.c
@@ -1,8 +1,8 @@
 /**
  * \file main.c
  * \brief Programme principal
- * \author  Mathias et Nicolas 
- * \version 1.0
+ * \author Nicolas
+ * \version 1.1
  * 
  */
 
@@ -13,17 +13,39 @@
 #include "donnees.h"
 #include <time.h>
 #include "accueil.h"
+#include "shop.h"
+
+// Fonctions auxiliaires pour la lisibilité de la fonction main
+
+static void play(SDL_Event *event, world_t *world, SDL_Renderer *renderer, textures_t *textures) {
+    handle_events(event, world);
+    update_data(world);
+    refresh_graphics(renderer, world, textures);
+    pause(17);
+}
+
+// Appelée quand on quitte le shop pour lancer la vague suivante
+static void start_next_wave(world_t *world) {
+    MiseAJour_Vague(world);
+    init_enemies(world);
+    world->state = STATE_PLAY;
+}
+
+static void menu(SDL_Event *event, world_t *world, SDL_Renderer *renderer, textures_t *textures, SDL_Window *window) {
+    (void)event; (void)renderer; (void)textures; (void)window;
+    world->state = STATE_PLAY;
+}
 
 /**
- *  \brief programme principal qui implémente la boucle du jeu
+ *  \brief Programme principal qui implémente la boucle du jeu
  */
 
 
 int main(int argc, char* args[]){
-    // Paramètres inutilisés
+    ///// Paramètres inutilisés /////
     (void)argc;
     (void)args;
-    //
+    ////////////////////////////////
 
     SDL_Event event;
     world_t world;
@@ -34,18 +56,36 @@ int main(int argc, char* args[]){
     srand(time(NULL)) ;
     //initialisation du jeu
     init(&window,&renderer,&textures,&world);
+
     printf("--------------------Debut Partie--------------------\n");
-    while(!is_game_over(&world)){ //tant que le jeu n'est pas fini
-        
-        //gestion des évènements
-        handle_events(&event,&world);
-        
-        //mise à jour des données liée à la physique du monde
-        update_data(&world);
-        //rafraichissement de l'écran
-        refresh_graphics(renderer,&world,&textures);
-        // pause de 10 ms pour controler la vitesse de rafraichissement
-        pause(17);
+    while(!is_game_over(&world)){ // tant que le jeu n'est pas fini
+
+        switch (world.state) {
+
+            case STATE_MENU:
+                menu(&event, &world, renderer, &textures, window);
+                break;
+
+            case STATE_PLAY:
+                play(&event, &world, renderer, &textures);
+                break;
+
+            case STATE_SHOP:
+                shop_run(window, renderer, &world, &textures);
+                if (world.state == STATE_PLAY) {
+                    start_next_wave(&world);
+                }
+                break;
+
+            case STATE_GAMEOVER:
+                world.gameover = 1;
+                break;
+
+            default:
+                // sécurité
+                world.gameover = 1;
+                break;
+        }
     }
     MessageVictoire(&world);
     GameOver(renderer, &textures);
